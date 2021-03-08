fc15634fe9fbee0cd5e28bb5872ecef4cc9623ea
Alvin Awuor <alvinawuor8@gmail.com>
2021-03-08 21:00:46 +0300

    Edit switch statement.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -4,46 +4,36 @@ $(document).ready(function(){
         e.preventDefault();
         var sizePrice = parseInt($("input[type='radio'][name='size']:checked").val());
         var crustPrice = parseInt($("#crust").val());
-
-        var tomatoes = parseInt($("#tomatoes").val());
-        var onions = parseInt($("#onions").val());
-        var avacados = parseInt($("#avacados").val());
-        var chilli = parseInt($("#chilli").val());
-        var cheese = parseInt($("#cheese").val());
-        var olives = parseInt($("#olives").val());
-        var pineapples = parseInt($("#pineapples").val());
-        var pepperoni = parseInt($("#pepperoni").val());
-        var chicken = parseInt($("#chicken").val());
-        var beef = parseInt($("#beef").val());
-        var bacon = parseInt($("#bacon").val());
-        var mushroom = parseInt($("#mushroom").val());
-
-        var toppings = [tomatoes, onions, avacados, chilli, cheese, olives, pineapples, pepperoni, chicken, beef, bacon, mushroom];
         
         switch (sizePrice) {
             case 400:
-                toppings.forEach(function(topping) {
-                    topping = 20;
-                    console.log(topping);
+                var toppings = [];
+                $("input[type='checkbox'][name='toppings']:checked").each(function (topping) {
+                    toppings[topping] = 20;
+                    // console.log(toppings[topping]);
                 });
                 break;
             
             case 700:
-                toppings.forEach(function(topping) {
-                    topping = 40;
-                    console.log(topping);
+                var toppings = [];
+                $("input[type='checkbox'][name='toppings']:checked").each(function (topping) {
+                    toppings[topping] = 40;
+                    // console.log(toppings[topping]);
                 });
                 break;
             
             case 1000:
-                toppings.forEach(function (topping) {
-                    topping = 60;
-                    console.log(topping);
+                var toppings = [];
+                $("input[type='checkbox'][name='toppings']:checked").each(function (topping) {
+                    toppings[topping] = 60;
+                    // console.log(toppings[topping]);
                 });
                 break;
         
             default:
                 break;
         }
+
+        // console.log(toppings[0]);
     });
 });
